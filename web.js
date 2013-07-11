39277853b2242987f758fb9a4d4f2c337c5a3e04
Syed Rakib Al Hasan <syed.rakib.al.hasan@gmail.com>
2013-07-12 02:18:36 +0600

    landing page will now read contents from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
+var fs = require('fs');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  indexHTML_buffer   = fs.readFileSync('index.html');
+  indexHTML_contents = indexHTML_buffer.toString();
+  response.send(indexHTML_contents);
 });
 
 var port = process.env.PORT || 5000;
